45c105dd9a9e2eb365fbd7cc668f23fbdd5a6d1d
santiago803 <santiago.londono13@udea.edu.co>
2025-07-01 15:50:26 -0500

    metodos

--- Codigo/obstaculos.h
+++ Codigo/obstaculos.h
@@ -7,26 +7,33 @@
 #include <QObject>
 #include <QGraphicsPixmapItem>
 #include <QTimer>
+#include <QtMath>
 class jugador;
 
 class obstaculos: public QGraphicsItem, public QObject
 {
 private:
-    int posx;
-    int posy;
-    int velx;
-    int vely;
-    int g;
-    int t;
+    float posx;
+    float posy;
+    float velx;
+    float vely;
+    float y0;
+    float t0;
+    float g;
+    float t;
     int ancho, alto;
     jugador* Goku;
     QTimer* timer;
     unsigned short tipo;
+    bool disponible;
 public:
-    obstaculos(int x, int y, int velox, int veloy, int tiempo, int gravedad, int anchoi, int altoi, jugador *Gokui, unsigned short tipo);
+    obstaculos(float x, float y, float velox, float veloy, float tiempo, float gravedad, int anchoi, int altoi, jugador *Gokui, unsigned short tipo);
     QRectF boundingRect() const override;
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr) override;
-
+    void desactivar();
+    void activar(QPointF posicion, int tiempo);
+    bool colision();
+    bool getdisponible();
 public slots:
     void moveeny();
     void moveSenoidal();
