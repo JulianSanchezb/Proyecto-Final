52f5e96c0e0fdc1e9937bbbc23192ca20c204c5b
JulianSanchezb <112278026+JulianSanchezb@users.noreply.github.com>
2025-07-07 14:27:42 -0500

    Menú avanzado

--- Codigo/menu.h
+++ Codigo/menu.h
@@ -4,13 +4,21 @@
 #include <QGraphicsPixmapItem>
 #include <QPushButton>
 #include <QGraphicsProxyWidget>
+#include <QObject>
+#include <QTimer>
+#include <QVector>
 
-class menu
+class menu : public QObject  // ← Heredar de QObject
 {
+    Q_OBJECT
 private:
     QGraphicsScene* scene;
     QPushButton* boton;
     QGraphicsProxyWidget* proxy1;
+    QVector<QPixmap> spritesRoshi;
+    QGraphicsPixmapItem* roshi;   // El objeto gráfico que se ve en la escena
+    QTimer* timer;
+    short int frameActual;
 public:
     menu();
     QGraphicsScene* obtenerEscena();
